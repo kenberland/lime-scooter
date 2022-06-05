77ed82da46ebdfdc5337d70a16b56cdb56fdf997
Ken Berland <berlandk@amazon.com>
2022-06-05 15:03:03 -0700

    add eeprom odometer

--- lime.ino
+++ lime.ino
@@ -1,15 +1,18 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
+#include <EEPROM.h>
 
 #define RX_BUFFER_SIZE 14
 #define TX_BYTE_SIZE 20
 #define PI 3.14159
+#define METERS_PER_KM 1000
 #define INTERVAL_IN_MS 100
 #define ONE_SECOND_IN_MS 1000
 #define MM_PER_METER 1000
 #define SECONDS_IN_ONE_HOUR 3600
 #define WHEEL_DIAMETER_IN_MM 254
+#define BLANKING_INTERVAL_IN_SEC 10
 
 #define TFT_CS -1
 #define TFT_RST        4 // D2 from RES pin on display
@@ -22,58 +25,83 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 float current_speed_in_meters_per_second = 0;
 float current_speed_in_km_per_hour;
 float last_speed_in_km_per_hour = 0;
-double current_odometer = 0;
+double current_odometer = 0; // in meters
+unsigned long eeprom_odometer_in_km = 0;
 double last_odometer = 0;
 float last_volts;
 
 unsigned long last_odometer_update_in_millis;
 unsigned long last_moving_time_in_millis;
 
-uint8_t screen_is_blanked = 0;
+uint8_t screen_is_blanked;
 
 void setup_tft() {
+  screen_is_blanked = 0;
+  last_moving_time_in_millis = millis();
   tft.init(240, 240, SPI_MODE3);
   tft.setSPISpeed(10000000);
   tft.setRotation(2);
   tft.fillScreen(ST77XX_BLACK);
 }
 
+void wake_tft() {
+  setup_tft();
+  draw_kmh_legend();
+  draw_battery_legend();
+  display_odometer(1);
+  set_screen_blank(0);
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(A0, INPUT);
-  setup_tft();
-  Serial.begin(9600);
-  last_odometer_update_in_millis = millis();
-  draw_kmh_legend();
-  draw_battery_legend();
-  display_zeroed_odometer();
   pinMode(15, OUTPUT);
-  set_screen_blank(0);
+  Serial.begin(9600);
+  EEPROM.begin(4);
+  wake_tft();
+  last_odometer_update_in_millis = millis();
+  read_odometer_from_eeprom();
 }
 
 void loop() {
   receive_controller_msg();
   send_controller_settings();
-  compute_odometer();
   float current_volts = get_battery_voltage();
   display_battery_voltage(current_volts);
-  display_odometer();
+  compute_odometer(current_volts);
+  display_odometer(0);
   display_speed();
+
   toggle_onboard_led();
-  if (last_moving_time_in_millis + ONE_SECOND_IN_MS * 10  < millis()) {
+  if (last_moving_time_in_millis + ONE_SECOND_IN_MS * BLANKING_INTERVAL_IN_SEC  < millis()) {
     set_screen_blank(1);
   } else if (screen_is_blanked) {
     set_screen_blank(0);
   }
+  if ( floor(current_odometer / METERS_PER_KM ) > eeprom_odometer_in_km) {
+      write_odometer_to_eeprom();
+    }
   last_speed_in_km_per_hour = current_speed_in_km_per_hour;
   last_odometer = current_odometer;
   last_volts = current_volts;
 
-//  char foobuffer[16];
-//  sprintf(foobuffer,"lmt: %d, b: %d", last_moving_time_in_millis, screen_is_blanked);
-//  display_error(foobuffer);
-
   delay(INTERVAL_IN_MS);
+}
+
+void write_odometer_to_eeprom() {
+  EEPROM.put(0, (unsigned long) ((unsigned long) floor(current_odometer / METERS_PER_KM) ));
+  EEPROM.commit();
+}
+
+void zero_odometer() {
+  unsigned long zero_odometer = 99;
+  EEPROM.put(0, zero_odometer);
+  EEPROM.commit();
+}
+
+void read_odometer_from_eeprom() {
+  EEPROM.get(0, eeprom_odometer_in_km);
+  current_odometer = eeprom_odometer_in_km * METERS_PER_KM;
 }
 
 void set_screen_blank(uint8_t is_blanked) {
@@ -112,7 +140,7 @@ bool valid_checksum(byte* frame_buf) {
 float get_battery_voltage() {
   unsigned int adc_voltage = analogRead(A0);
   float volts = adc_voltage / 1023.0;
-  return (volts * 40);
+  return (volts * 38);
 }
 
 unsigned int led = 0;
@@ -137,7 +165,7 @@ void send_controller_settings() {
   unsigned int bytes_tx;
   bytes_tx = Serial.write(tx_buf, TX_BYTE_SIZE);
   if (bytes_tx != TX_BYTE_SIZE) {
-    display_tx_error(bytes_tx);
+    //display_tx_error(bytes_tx);
   }
 }
 
@@ -158,22 +186,20 @@ void receive_controller_msg() {
           last_moving_time_in_millis = millis();
       } else {
         drain_serial();
-        display_checksum_error();
+        //        display_checksum_error();
       }
       rx_idx = 0;
     }
   }
 }
 
-void compute_odometer() {
+void compute_odometer(float current_volts) {
   unsigned long now_in_millis = millis();
   unsigned long duration_in_millis = now_in_millis - last_odometer_update_in_millis;
   float duration_in_seconds = (float) duration_in_millis / ONE_SECOND_IN_MS;
-  current_odometer = current_odometer + current_speed_in_meters_per_second * duration_in_seconds;
+  if ( (int) current_volts > 5) // is the scooter on?
+    current_odometer = current_odometer + current_speed_in_meters_per_second * duration_in_seconds;
   last_odometer_update_in_millis = now_in_millis;
-  //  char buffer[256];
-  //  sprintf(buffer, "ms: %.2f, d: %.4f", current_speed_in_meters_per_second, duration_in_seconds);
-  //  display_error(buffer);
 }
 
 #define SPEED_CHAR1_OFFSET 40
@@ -202,6 +228,9 @@ void draw_battery_legend() {
 }
 
 void display_battery_voltage(float current_volts) {
+  if (current_volts >= last_volts + 10 ||
+      current_volts <= last_volts - 10  )
+    wake_tft(); // turning it on or off fucks up the SPI. Maybe it needs shielding?
   tft.setTextSize(3);
   char buffer[8];
   if ((int) current_volts != (int) last_volts) {
@@ -215,7 +244,7 @@ void display_battery_voltage(float current_volts) {
   sprintf(buffer, "%02d", (int) current_volts);
   tft.print(buffer);
 }
-void display_zeroed_odometer(void) {
+void XXX_init_odometer(void) {
   unsigned int offset = 0;
   tft.setTextSize(3);
   tft.setTextColor(ST77XX_CYAN);
@@ -226,8 +255,8 @@ void display_zeroed_odometer(void) {
   }
 }
 
-void display_odometer(void) {
-  if  ( (unsigned long) current_odometer == (unsigned long) last_odometer )
+void display_odometer(uint8_t is_init) {
+  if  ( !is_init && (unsigned long) current_odometer == (unsigned long) last_odometer )
     return;
 
   double current_odometer_copy = current_odometer;
@@ -247,7 +276,7 @@ void display_odometer(void) {
     unsigned int current_odometer_sd = current_odometer_copy / pow(10, idx);
     current_odometer_previous_msd = current_odometer_sd * pow(10, idx);
 
-    if (current_odometer_sd != last_odometer_sd ) {
+    if (is_init || current_odometer_sd != last_odometer_sd ) {
       tft.setTextSize(3);
       // erase old digit
       tft.setTextColor(ST77XX_BLACK);
@@ -300,7 +329,9 @@ void display_error(char *msg) {
   tft.setTextColor(ST77XX_WHITE);
   tft.setCursor(0, 0);
   char buffer[24];
-  sprintf(buffer, "%s", msg);
+  char small_buffer[24];
+  strncpy(small_buffer, msg, 24 - 1);
+  sprintf(buffer, "%s", small_buffer);
   tft.print(buffer);
 }
 
